b7080f9fb61a55f813e3613baf9849a7c31fdb44
Frances Crouch <Frankie@Francess-MacBook-Pro.local>
2017-09-17 09:07:44 +0100

    Remove DS_store and jiggle files

--- code to submit/Scripts/Parallel BCART/BCART/__init__.py
+++
@@ -1,4 +0,0 @@
-from ParallelBCART_BayesianTree import Node, Tree
-from ParallelBCART_initialSample import build_tree
-from ParallelBCART_proposal import tree_proposal
-from ParallelBCART_evaluation import calc_auc
